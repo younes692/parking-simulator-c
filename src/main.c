6e581eee2d11574c712892f91a47413a95215842
younes <younes692@users.noreply.github.com>
2026-05-20 14:05:29 +0000

    init

---
+++ src/main.c
@@ -0,0 +1,20 @@
+/*
+ * TP Parking - Simulation de parking avec threads
+ * Auteur  : Younes B.
+ * Date    : 2024-02-05
+ * Cours   : Systemes d'exploitation - L2 Info
+ *
+ * On simule un parking avec N places et M voitures
+ * chaque voiture est un thread qui essaye d'entrer dans le parking
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "parking.h"
+
+int main(int argc, char* argv[]) {
+    // TODO: creer les threads voitures
+    printf("debut simulation parking\n");
+    return 0;
+}
