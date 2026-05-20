bdc356d9d8463ee6fa4556636b9ae4efdf3979d5
younes <younes692@users.noreply.github.com>
2026-05-20 12:19:32 +0000

    init

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ * Projet : Simulation du Probleme de Parking
+ * Auteur : Younes B.
+ * Date   : 2024-02-01
+ * Cours  : Systemes d'exploitation - L2 Info
+ *
+ * Simulation d'un parking avec threads POSIX
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "parking.h"
+
+int main() {
+    // TODO: creer les threads voitures
+    printf("Simulation parking - debut\n");
+    return 0;
+}
